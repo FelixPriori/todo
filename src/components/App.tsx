e1b7b57bacab160e614a09934c97472be0f104e6
felix <felix@webisoft.com>
2020-11-02 17:02:35 -0500

    basic title

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="max-w-md mx-auto flex flex-col p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+      <h1 className="title">Todo</h1>
+    </div>
+  )
+}
+
+export default App
